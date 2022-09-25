fa6fa092aa8de923f5e826622a6beebafc5bce96
Chacha Nurholis <chachanurholis29@gmail.com>
2022-09-25 19:54:02 +0700

    feat: add footer layout

---
+++ layouts/public/FooterLayout.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="bg-black text-white p-5">
+        <h1 class="text-center">Footer</h1>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    name: "FooterLayout"
+}
+</script>
