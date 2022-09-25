74502d9727ba1dd6e3010e3fbfa5b84d54f024ea
Chacha Nurholis <chachanurholis29@gmail.com>
2022-09-25 19:53:38 +0700

    feat: add login container

---
+++ containers/LoginContainer.vue
@@ -0,0 +1,14 @@
+<template>
+    <MainLayout>
+        <h1>Login Page</h1>
+    </MainLayout>
+</template>
+
+<script lang="ts">
+import MainLayout from '~/layouts/public/MainLayout.vue';
+
+export default {
+    name: "LoginContainer",
+    components: { MainLayout },
+}
+</script>
