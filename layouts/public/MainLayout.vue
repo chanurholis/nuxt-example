4409e4f8c9b2ca1ade0dccee8f67a0e7cb6e5d84
Chacha Nurholis <chachanurholis29@gmail.com>
2022-09-25 19:54:20 +0700

    feat: add main layout

---
+++ layouts/public/MainLayout.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <HeaderLayout/>
+        <div class="container mx-auto p-6">
+            <slot />
+        </div>
+        <FooterLayout/>
+    </div>
+</template>
+
+<script lang="ts">
+import HeaderLayout from './HeaderLayout.vue';
+import FooterLayout from './FooterLayout.vue';
+
+export default {
+    name: "MainLayout",
+    components: { HeaderLayout, FooterLayout }
+}
+</script>
