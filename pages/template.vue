b9efa00949e361ed2f2e633376cbfbbbae1a1f12
Chacha Nurholis <chachanurholis29@gmail.com>
2022-09-25 19:54:51 +0700

    feat: add template

---
+++ pages/template.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container p-8">
+        <h1 class="text-center py-2 font-bold">Template</h1>
+        <br>
+        <div>
+            <h2>Button</h2>
+            <ButtonComponent name="Primary" type="primary" />
+            <ButtonComponent name="Success" type="success" />
+            <ButtonComponent name="Secondary" type="secondary" />
+            <ButtonComponent name="Warning" type="warning" />
+            <ButtonComponent name="Danger" type="danger" />
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import ButtonComponent from '~/components/ButtonComponent.vue';
+
+export default {
+    name: "TemplatePage",
+    components: { ButtonComponent }
+}
+</script>
