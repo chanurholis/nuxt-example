d11e7385807742af0edeff76752c3b8fa4e461f2
Chacha Nurholis <chachanurholis29@gmail.com>
2022-09-25 19:54:44 +0700

    feat: add login page

---
+++ pages/login.vue
@@ -0,0 +1,12 @@
+<template>
+    <LoginContainer />
+</template>
+
+<script lang="ts">
+import LoginContainer from '~/containers/LoginContainer.vue';
+
+export default {
+    name: "LoginPage",
+    components: { LoginContainer }
+}
+</script>
