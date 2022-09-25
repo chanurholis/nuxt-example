ecf010fec72777a1b410e29cb4d3efe5e4e79ab3
Chacha Nurholis <chachanurholis29@gmail.com>
2022-09-25 19:54:36 +0700

    feat: add landing page

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <MainContainer />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+import MainContainer from '~/containers/MainContainer.vue';
+
+export default Vue.extend({
+  name: "IndexPage",
+  components: { MainContainer }
+})
+</script>
