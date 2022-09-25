48b8f40be9e1a5210ae4256b3e01eef2eb7c9543
Chacha Nurholis <chachanurholis29@gmail.com>
2022-09-25 19:53:45 +0700

    feat: add main container

---
+++ containers/MainContainer.vue
@@ -0,0 +1,14 @@
+<template>
+    <MainLayout>
+        <h1>Landing Page</h1>
+    </MainLayout>
+</template>
+
+<script lang="ts">
+import MainLayout from '~/layouts/public/MainLayout.vue';
+
+export default {
+    name: "MainContainer",
+    components: { MainLayout }
+}
+</script>
